f527f0fe623c328f36306530b0e2efd1a9aa8804
Sergio <winksousa0@gmail.com>
2023-02-02 21:23:22 -0300

    add libs and global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
+import logo from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "src/styles/pages/app";
 
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} width={130} height={52} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
